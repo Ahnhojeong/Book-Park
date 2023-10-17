f0cbe6e272b37f19ff9f175271333ac94511ee38
Hojeong Ahn <spdlqj427@gmail.com>
2023-10-17 23:03:10 +0900

    ✨  pages 추가

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import BodyLayout from "@components/layout/BodyLayout";
+
+function HomePage() {
+  return <BodyLayout>HomePage</BodyLayout>;
+}
+
+export default HomePage;
---
+++ src/pages/LibraryPage.tsx
@@ -0,0 +1,7 @@
+import BodyLayout from "@components/layout/BodyLayout";
+
+function LibraryPage() {
+  return <BodyLayout>LibrarayPage</BodyLayout>;
+}
+
+export default LibraryPage;
