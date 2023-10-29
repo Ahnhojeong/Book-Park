271a60202559c227666d893a7a37f8a24b073133
Hojeong Ahn <spdlqj427@gmail.com>
2023-10-29 23:22:34 +0900

    💥 API instance 설정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,14 +1,21 @@
 import axios from "axios";
 
 // common config
-axios.defaults.headers.common["Accept"] = "application/json";
+// axios.defaults.headers.common["Accept"] = "application/json";
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 const curtureApi = axios.create({
   baseURL: process.env.REACT_APP_CURTURE_SERVER,
 });
 
 const libraryPlazaApi = axios.create({
-  baseURL: process.env.REACT_APP_LIBRARY_BIGDATA_SERVER,
+  // baseURL: process.env.REACT_APP_LIBRARY_BIGDATA_SERVER,
+  baseURL: "data4library",
+});
+
+const libraryPlazaApi2 = axios.create({
+  // baseURL: process.env.REACT_APP_LIBRARY_BIGDATA_SERVER,
+  baseURL: "/book/data4library",
 });
 
 curtureApi.interceptors.request.use(
@@ -31,4 +38,4 @@ libraryPlazaApi.interceptors.request.use(
   }
 );
 
-export { curtureApi, libraryPlazaApi };
+export { curtureApi, libraryPlazaApi, libraryPlazaApi2 };
